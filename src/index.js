a6dd404edfe939f4550d3f02b7ce6c3aa4a969c0
Nancy Chaudhry <nancych14@gmail.com>
2023-01-30 19:59:21 +0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import './styles/style.css';
+import Todo from "./Todo";
+import addIcon from './assets/add-solid.svg';
+import projectIcon from './assets/folder.svg';
+
+const openTodoForm = document.querySelector('.openTodoForm');
+const todoModal = document.querySelector('.todoModal');
+const todoForm = document.querySelector('#todo-form');
+const newProjectBtn = document.querySelector('#newProjectBtn');
+const projectForm = document.querySelector('.projectForm');
+const projectFormBtn = document.querySelector('#projectFormBtn');
+const projectArray = [];
+const projectList = document.querySelector('.project-list');
+
+// Open the Project form in sidebar
+newProjectBtn.addEventListener('click', () => {
+    projectForm.classList.toggle('hide');
+	projectForm.classList.toggle('show');
+});
+
+projectFormBtn.addEventListener('click', (e) => {
+    projectForm.classList.toggle('hide');
+	projectForm.classList.toggle('show');
+    e.preventDefault();
+    let projectTitle = document.querySelector('#project-title');
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.innerText = projectTitle.value;
+    projectArray.push(projectTitle.value);
+    li.appendChild(a);
+    projectList.appendChild(li);
+    projectTitle.value = '';
+});
+
+openTodoForm.addEventListener('click', () => {
+	todoModal.showModal();
+});
+
+// Event listener for Todo form
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const title = document.querySelector('#title').value;
+    const description = document.querySelector('#description').value;
+    const priority = document.querySelector('#priority').value;
+    const completed = false;
+    
+    const todo = new Todo(
+        title,
+        description,
+        dueDate,
+        priority,
+        completed,
+    );
+    console.log(todo);
+    todoModal.close();
+});
+
